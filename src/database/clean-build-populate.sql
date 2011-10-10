7d538c512b4d85c53bb3bef519382b2753b9d2ec
foguinho.peruca@gmail.com <foguinho.peruca@gmail.com@2b881542-9fa8-11dd-9a14-c1645b63b0c6>
2011-10-10 16:29:45 +0000

    refs #1 * Removing old files - user lib * added maven to manage awknet-commons
    git-svn-id: https://awknet.googlecode.com/svn/trunk@17 2b881542-9fa8-11dd-9a14-c1645b63b0c6

---
+++ src/database/clean-build-populate.sql
@@ -0,0 +1,9 @@
+-- Don't forget: Modify the path for all lines! To use shell type:
+-- $ mysql --default-character-set UTF8 -usigerar -pA12345678a -e"Source /home/jefferson/Projects/sigerar/trunck/docs/model/database/sigerar.sql"
+-- Or if you want to make it more sweet use this:
+-- $ alias sigerar_db='mysql --default-character-set UTF8 -usigerar -pA12345678a -e"Source /home/jefferson/Projects/sigerar/trunck/docs/model/database/sigerar.sql"'
+-- $ sigerar_db
+-- Add it to the end of your ~/.bashrc !
+-- Have fun! - Jeff - jcampos [at] sigerar [dot] org
+source /home/jefferson/universal/projects/sigerar/branches/v2/src/scripts/db/CREATE_SIGERAR.sql
+source /home/jefferson/universal/projects/sigerar/branches/v2/src/scripts/db/INSERT_SIGERAR.sql
