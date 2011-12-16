b307b77bd7f870e233e32489d0bd432f2d35845d
foguinho.peruca@gmail.com <foguinho.peruca@gmail.com@2b881542-9fa8-11dd-9a14-c1645b63b0c6>
2011-12-16 16:00:46 +0000

    Trigger - initail creation;
    git-svn-id: https://awknet.googlecode.com/svn/trunk@48 2b881542-9fa8-11dd-9a14-c1645b63b0c6

--- awknet-commons/src/main/database/DML_awknet-commons.sql
+++ awknet-commons/src/main/database/DML_awknet-commons.sql
@@ -19,8 +19,17 @@
 SET CHARACTER SET UTF8;
 
 INSERT INTO TUser (ID, login, password, email) VALUES
-    (1, '95310731393', '229c3f7e7b9c1be5bfa2f46d90c4ab00', 'jecampos@sorocaba.sp.gov.br'),
+    (1, '95310731393', '229c3f7e7b9c1be5bfa2f46d90c4ab00', 'root@awknet.org'),
     (2, '66997284825', '229c3f7e7b9c1be5bfa2f46d90c4ab00', 'somebody@awknet.org'),
     (3, '84358568360', '229c3f7e7b9c1be5bfa2f46d90c4ab00', 'simple@awknet.org'),
     (4, '02598649435', '229c3f7e7b9c1be5bfa2f46d90c4ab00', 'someone@awknet.org')
     ;
+
+delimiter $$
+CREATE TRIGGER invalidateOldRetrieveCode BEFORE INSERT ON TRetrieve_Password_LOG
+	FOR EACH ROW 
+	BEGIN
+		UPDATE TRetrieve_Password_LOG AS rc 
+			SET rc.updated = TRUE 
+			WHERE rc.userID = NEW.userID; 
+	END $$
