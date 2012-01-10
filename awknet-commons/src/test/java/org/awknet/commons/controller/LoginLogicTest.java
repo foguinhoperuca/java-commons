eb193d9f79d9922810018c31448363f00a87cea9
foguinho.peruca@gmail.com <foguinho.peruca@gmail.com@2b881542-9fa8-11dd-9a14-c1645b63b0c6>
2012-01-10 16:57:56 +0000

    Created the test for login
    git-svn-id: https://awknet.googlecode.com/svn/trunk@64 2b881542-9fa8-11dd-9a14-c1645b63b0c6

--- awknet-commons/src/test/java/org/awknet/commons/controller/LoginLogicTest.java
+++ awknet-commons/src/test/java/org/awknet/commons/controller/LoginLogicTest.java
@@ -23,70 +23,75 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.awknet.commons.data.DaoFactory;
-import org.awknet.commons.model.entity.RetrievePasswordLog;
 import org.awknet.commons.model.entity.User;
 import org.junit.Before;
 import org.junit.Test;
 
 public class LoginLogicTest {
+    private static String DEFAULT_PASSWORD_ENCRYPT = "229c3f7e7b9c1be5bfa2f46d90c4ab00";
+    private static String DEFAULT_PASSWORD = "A12345678a";
+    private LoginLogic instance;
+    private DaoFactory daoFactory;
+    private User user;
+    private User user_invalid;
+    private List<String> retrieveCodes;
 
-	private LoginLogic instance;
-	private DaoFactory daoFactory;
-	private User user;
-	private List<String> retrieveCodes;
+    @Before
+    public void setUp() throws Exception {
+	daoFactory = new DaoFactory();
+	instance = new LoginLogic(daoFactory);
+	user = new User("95310731393", DEFAULT_PASSWORD);
+	user_invalid = new User("1597536485", DEFAULT_PASSWORD);
+	// for (RetrievePasswordLog rpLog :
+	// daoFactory.getRetrievePasswordLogDao()
+	// .list())
+	// retrieveCodes.add(rpLog.getRetrieveCode());
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		daoFactory = new DaoFactory();
-		instance = new LoginLogic(daoFactory);
-		user = new User();
-		for (RetrievePasswordLog rpLog : daoFactory.getRetrievePasswordLogDao()
-				.list())
-			retrieveCodes.add(rpLog.getRetrieveCode());
-	}
+    @Test
+    public void testDoLogin() {
+	System.out.println("========== TEST DO LOGIN ==========");
+	assertEquals("ok", instance.doLogin(user));
+	assertEquals("invalid", instance.doLogin(user_invalid));
+    }
 
-	// @Test
-	// public void testLoginLogic() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testLogin() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testDoLogin() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testLogout() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testRetrievePassword() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testRetrieveCodeValidation() {
-	// fail("Not yet implemented");
-	// }
-	//
-	// @Test
-	// public void testUpdatePassword() {
-	// for (String rc : retrieveCodes)
-	// assertEquals("ok", instance.updatePassword("newPassword", rc));
-	//
-	// assertEquals("error",
-	// instance.updatePassword("newPassword", "invalid_retrieveCode"));
-	// }
-	//
-	// @Test
-	// public void testGetLogin() {
-	// fail("Not yet implemented");
-	// }
+    // @Test
+    // public void testLoginLogic() {
+    // fail("Not yet implemented");
+    // }
+    //
+    // @Test
+    // public void testLogin() {
+    // fail("Not yet implemented");
+    // }
+    //
+    // @Test
+    // public void testLogout() {
+    // fail("Not yet implemented");
+    // }
+    //
+    // @Test
+    // public void testRetrievePassword() {
+    // fail("Not yet implemented");
+    // }
+    //
+    // @Test
+    // public void testRetrieveCodeValidation() {
+    // fail("Not yet implemented");
+    // }
+    //
+    // @Test
+    // public void testUpdatePassword() {
+    // for (String rc : retrieveCodes)
+    // assertEquals("ok", instance.updatePassword("newPassword", rc));
+    //
+    // assertEquals("error",
+    // instance.updatePassword("newPassword", "invalid_retrieveCode"));
+    // }
+    //
+    // @Test
+    // public void testGetLogin() {
+    // fail("Not yet implemented");
+    // }
 
 }
